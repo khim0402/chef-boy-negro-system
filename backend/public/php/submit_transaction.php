18bc5aa08f41833ead6d9740f5dcb0f4a7e5fe9f
Khim <khimsantos97@gmail.com>
2025-11-27 04:40:34 +0800

    fix

--- backend/public/php/submit_transaction.php
+++ backend/public/php/submit_transaction.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-require_once(__DIR__ . '/../../php/db.php');
+require_once(__DIR__ . '/db.php');
 
 try {
     $input = json_decode(file_get_contents('php://input'), true);
