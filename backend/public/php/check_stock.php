4da7a6a8a723386e66b9b7ccbff3db4435b8ca62
Khim <khimsantos97@gmail.com>
2025-11-27 04:27:41 +0800

    fix again

--- backend/public/php/check_stock.php
+++ backend/public/php/check_stock.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once(__DIR__ . '/../../php/db.php');
+require_once(__DIR__ . '/../db.php');
 
 $data = json_decode(file_get_contents("php://input"), true);
 if (!$data) {
