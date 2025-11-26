303035a9a4beed6f748aaeeb8806a25bacdeee01
Khim <khimsantos97@gmail.com>
2025-11-27 04:19:46 +0800

    fix

--- backend/php/db.php
+++ backend/php/db.php
@@ -7,7 +7,6 @@ $password = getenv("PGPASSWORD");
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo = new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
